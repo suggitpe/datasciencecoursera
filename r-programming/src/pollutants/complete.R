a651da8fca3db38b197c91adba87218e3aa49b49
Peter Suggitt <me@suggs.org.uk>
2014-05-07 12:11:58 +0100

    completed polltant and complete

--- r-programming/src/pollutants/complete.R
+++ r-programming/src/pollutants/complete.R
@@ -1,8 +1,7 @@
-source('ReadData.R')
+source('readdata.R')
 
 complete <- function(directory, id = 1:332){
     print(paste("Running complete with:", directory, separator=" "))
-    fullDataForRange <- extractSubsetDataFrom(directory, id)
-    subsetOfData <- fullDataForRange[complete.cases(fullDataForRange),]
-    nrow(subsetOfData)
+    fullDataForRange <- createRangedDataframeFromFilesIn(directory, id)
+    sum(complete.cases(fullDataForRange))
 }
--- r-programming/src/pollutants/pollutantmean.R
+++ r-programming/src/pollutants/pollutantmean.R
@@ -1,8 +1,8 @@
-source('ReadData.R')
+source('readdata.R')
 
 pollutantmean <- function( directory, pollutant, id = 1:332) {
     print(paste("Running polutantmean with:", directory, pollutant, separator=" "))
-    fullDataForRange <- extractSubsetDataFrom(directory, id)
+    fullDataForRange <- createRangedDataframeFromFilesIn(directory, id)
     subsetOfData <- subset(fullDataForRange, !is.na(fullDataForRange[,pollutant]))    
     mean(subsetOfData[,pollutant])
 }
