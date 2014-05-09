67c49b5e92c6788fa884d604b39d773350f29923
Peter Suggitt <me@suggs.org.uk>
2014-05-09 18:41:36 +0100

    Updated functions with their function specification

--- r-programming/src/pollutants/complete.R
+++ r-programming/src/pollutants/complete.R
@@ -1,6 +1,20 @@
 source('readdata.R')
 
 complete <- function(directory, id = 1:332){
+    ## 'directory' is a character vector of length 1 indicating
+    ## the location of the CSV files
+    
+    ## 'id' is an integer vector indicating the monitor ID numbers
+    ## to be used
+    
+    ## Return a data frame of the form:
+    ## id nobs
+    ## 1  117
+    ## 2  1041
+    ## ...
+    ## where 'id' is the monitor ID number and 'nobs' is the
+    ## number of complete cases
+    
     print(paste("Running complete with:", directory, separator=" "))
     data <- createRangedDataframeFromFilesIn(directory, id)
     dataSummary <- tapply(complete.cases(data), data$ID, FUN=sum)
--- r-programming/src/pollutants/pollutantmean.R
+++ r-programming/src/pollutants/pollutantmean.R
@@ -1,6 +1,19 @@
 source('readdata.R')
 
 pollutantmean <- function( directory, pollutant, id = 1:332) {
+    ## 'directory' is a character vector of length 1 indicating
+    ## the location of the CSV files
+    
+    ## 'pollutant' is a character vector of length 1 indicating
+    ## the name of the pollutant for which we will calculate the
+    ## mean; either "sulfate" or "nitrate".
+    
+    ## 'id' is an integer vector indicating the monitor ID numbers
+    ## to be used
+    
+    ## Return the mean of the pollutant across all monitors list
+    ## in the 'id' vector (ignoring NA values)
+    
     print(paste("Running polutantmean with:", directory, pollutant, separator=" "))
     fullDataForRange <- createRangedDataframeFromFilesIn(directory, id)
     subsetOfData <- subset(fullDataForRange, !is.na(fullDataForRange[,pollutant]))    
