6f0a66b0fa5e1c0504b6d2e93ce77f9caff8d361
Peter Suggitt <me@suggs.org.uk>
2014-06-02 19:39:39 +0100

    refactored the read data to encapsulate the read, validate, sort functions

--- r-programming/src/hospitals/best.R
+++ r-programming/src/hospitals/best.R
@@ -1,21 +1,9 @@
 source('readdata.R')
 
 best <- function(state, outcome) {
-    data <- validateData(readData(), state, outcome)
-    data <- sortDataFor(data, outcome)
+    data <- readAndSortDataFor(state, outcome)
     data[1,1]    
 }
 
 
 
-sortDataFor <- function(data, outcome){
-    if(outcome == "heart attack"){
-        data[order(data[3], data[1]),]
-    }
-    else if(outcome == "heart failure"){
-        data[order(data[4], data[1]),]
-    }
-    else if(outcome == "pneumonia"){
-        data[order(data[5], data[1]),]
-    }    
-}
--- r-programming/src/hospitals/readdata.R
+++ r-programming/src/hospitals/readdata.R
@@ -1,26 +1,44 @@
-readData <- function(){
-    data <- read.csv('data/outcome-of-care-measures.csv', colClasses='character')
-    data <- data[,c(2,7,11,17,23)]
-    names(data) <- c('name', 'state', 'heart attack', 'heart failure', 'pneumonia')
+readAndSortDataFor<- function(state, outcome){
     
-    data[, c(3,4,5)] <- sapply(data[, c(3,4,5)], as.numeric)
-    data
-}
-
-validateData <- function(dataToValidate, theState, theOutcome){
-    
-    if(!(theOutcome %in% names(dataToValidate))){
-        stop("invalid outcome")
+    readData <- function(){
+        data <- read.csv('data/outcome-of-care-measures.csv', colClasses='character')
+        data <- data[,c(2,7,11,17,23)]
+        names(data) <- c('name', 'state', 'heart attack', 'heart failure', 'pneumonia')
+        
+        data[, c(3,4,5)] <- sapply(data[, c(3,4,5)], as.numeric)
+        data
     }
     
-    if(!(theState %in% dataToValidate[, "state"])){
-        stop("invalid state")
+    validateData <- function(dataToValidate, theState, theOutcome){
+        if(!(theOutcome %in% names(dataToValidate))){
+            stop("invalid outcome")
+        }
+        
+        if(!(theState %in% dataToValidate[, "state"])){
+            stop("invalid state")
+        }
+        
+        removeAllOtherStateData(dataToValidate, theState)
     }
     
-    removeAllOtherStateData(dataToValidate, theState)
-}
-
-removeAllOtherStateData <- function(data, stateToKeep){
-    data <- subset(data, data$state == stateToKeep)
+    removeAllOtherStateData <- function(data, stateToKeep){
+        data <- subset(data, data$state == stateToKeep)
+        data
+    }
+    
+    sortDataFor <- function(data, outcome){
+        if(outcome == "heart attack"){
+            data[order(data[3], data[1]),]
+        }
+        else if(outcome == "heart failure"){
+            data[order(data[4], data[1]),]
+        }
+        else if(outcome == "pneumonia"){
+            data[order(data[5], data[1]),]
+        }    
+    }
+    
+    data <- validateData(readData(), state, outcome)
+    data <- sortDataFor(data, outcome)
     data
 }
